581ce7c2084a88e3b78dbca1aaa7e9198d38a84c
jmin4078 <wpalsrl8588@gmail.com>
2026-08-20 10:50:51 +0900

    Add JWT configuration dependencies

--- src/main/java/org/example/jwtfetch/config/AuthProperties.java
+++ src/main/java/org/example/jwtfetch/config/AuthProperties.java
@@ -2,8 +2,15 @@ package org.example.jwtfetch.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import javax.xml.datatype.Duration;
+
 @ConfigurationProperties(prefix = "app.auth")
 public record AuthProperties(
-        String encodingId
+        String encodingId,
+        Jwt jwt
 ) {
+    public record Jwt(
+            String secretKey,
+            Duration accessTokenExpiry) {
+    }
 }
